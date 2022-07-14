05576292fcf65784633e026d9857a4f791e7e096
hillaryomondi <hillary.okoth@strathmore.edu>
2022-07-14 19:46:06 +0300

    Created and implemented delete, error, and logout dialogs
    Fixed the views errors

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,8 @@ import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/show_error_dialog.dart';
 
+import '../utilities/dialogs/error_dialog.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
 
